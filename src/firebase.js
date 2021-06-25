92404f167cd6ccc0c0eb8345a20171984ee5e19c
Sreehari Prathap <sreehariprathap1996@gmail.com>
2021-06-26 02:23:01 +0530

    finished frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeQcV_lRtQsSwSRsK26028s6akM33ATck",
+    authDomain: "netflix-clone-7d95c.firebaseapp.com",
+    projectId: "netflix-clone-7d95c",
+    storageBucket: "netflix-clone-7d95c.appspot.com",
+    messagingSenderId: "780433366695",
+    appId: "1:780433366695:web:b3d086b486746037e58582"
+  }; 
